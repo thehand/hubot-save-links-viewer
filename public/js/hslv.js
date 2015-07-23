9aa36efcb2af091dd7b0906439d90106c673b523
Francesco Salamone <salamone.francesco@gmail.com>
2015-07-24 01:06:08 +0200

    UI changed from MaterialDesign (AngularJS Module) to Twitter Bootstrap 3, some minor improvements

--- public/js/hslv.js
+++ public/js/hslv.js
@@ -1,28 +1,33 @@
-var app = angular
-  .module('hsl_viewer', ['ngMaterial'])
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-    .primaryPalette('teal')
-    .accentPalette('orange');
-  }
-);
+var app = angular.module('hsl_viewer', []);
+
+app.filter('searchFor', function() {
+  return function (arr,searchString) {
+    if (!searchString) {
+      return arr;
+    }
+
+    var result = [];
+    searchString = searchString.toLowerCase();
+
+    angular.forEach(arr, function (item) {
+      if (JSON.stringify(item).match(searchString)) result.push(item);
+    });
+
+    return result;
+  };
+});
 
 app.controller('LinksCtrl', [ '$scope', '$http', function($scope, $http) {
   $scope.sourceUrl = "http://gruspbot.herokuapp.com/links";
-  //$scope.sourceUrl = "/links";
   $scope.links = new Array();
   $scope.totalLinks = 0;
-	$scope.filterVal = null;
-	$scope.showFilter = false;
+  $scope.filterVal = null;
+  $scope.showFilter = false;
 
-	this.toggleFilter = function () {
-		$scope.showFilter = !$scope.showFilter;
-		return $scope.showFilter;
-	};
-
-	this.linkMatch = function (quickFilterText) {
-		return quickFilterText.match("/" + filterVal + "/i");
-	};
+  this.toggleFilter = function () {
+    $scope.showFilter = !$scope.showFilter;
+    return $scope.showFilter;
+  };
 
   this.getLinks = function () {
     var request = $http({
@@ -31,7 +36,6 @@ app.controller('LinksCtrl', [ '$scope', '$http', function($scope, $http) {
     }).
     success(function(res) {
       $scope.links = res.data;
-			$scope.links.quickFilterText = $scope.links.room + "-" + $scope.links.postedBy + "-" + $scope.links.link + "-" + $scope.links.tags;
       $scope.totalLinks = res.totalLinks;
     }).error(function(data, status, headers, config) {
       console.log(data);
@@ -41,5 +45,5 @@ app.controller('LinksCtrl', [ '$scope', '$http', function($scope, $http) {
     });
   };
 
-  //this.getLinks();
+  this.getLinks();
 }]);
